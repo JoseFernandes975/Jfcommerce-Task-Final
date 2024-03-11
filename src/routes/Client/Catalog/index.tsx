61ffca8b02075cb3da7de477f20ceeb2fd41c811
JoseFernandes975 <zehenrique752005@gmail.com>
2024-03-11 16:56:59 -0300

    Searching for products from the backend And dynamic listing

--- src/routes/Client/Catalog/index.tsx
+++ src/routes/Client/Catalog/index.tsx
@@ -6,35 +6,28 @@ import * as productService from '../../../services/product-service';
 
 export default function Catalog(){
 
-   const p1 : ProductDTO = {
-        id: 1,
-        name: "Computer",
-        price: 2000,
-        imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg'
-    }
+    const [products, setProducts] = useState<ProductDTO[]>();
 
-    const p2 : ProductDTO = {
-        id: 2,
-        name: "Computer",
-        price: 2000,
-        imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg'
-    }
-    const p3 : ProductDTO = {
-        id: 3,
-        name: "Computer",
-        price: 2000,
-        imgUrl: 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg'
-    }
+    useEffect(() => {
+      productService.findAllProducts().then(response => {
+        setProducts(response.data.content);
+      })
+     
+    }, [])
+
 
     return(
          <main>
             <section id="jf-section-catalog" className="jf-container">
              
               <div className='jf-container-products-cards'>
-                 
-                <CatalogCard product={p1} key={p1.id} />
-                 <CatalogCard product={p2} key={p2.id} />
-                 <CatalogCard product={p3} key={p3.id} />
+              {
+                    products
+                    &&
+                    products.map(x => <CatalogCard key={x.id} product={x} />)
+                }
+
+                
 
              </div>
              
